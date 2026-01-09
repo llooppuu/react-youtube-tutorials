041d74fb926b20134e319739622e50cdb717145b
Lauri <lauri.vosmi@voco.ee>
2026-01-09 10:27:50 +0200

    Added student and button

---
+++ src/button.jsx
@@ -0,0 +1,14 @@
+function Button(){
+
+    const styles = {
+        backgroundColor: 'blue',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer'
+    };
+
+    return (<button style={styles}>Click me</button>);
+}
+export default Button;
